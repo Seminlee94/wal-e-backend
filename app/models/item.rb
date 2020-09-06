b906ef3e61535e5c4c71fe203473e18f2ade2de7
Se min Lee <mlsml94@gmail.com>
2020-09-06 19:52:53 -0400

    api calls

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,6 @@ class Item < ApplicationRecord
     @@key = ENV["WEGMAN_API_KEY"]
 
     def self.category
-        # binding.pry
         # response = RestClient.get("https://api.wegmans.io/products/categories?api-version=2018-10-18", headers={'Subscription-Key': key}){|response, request, result| response }
         response = HTTParty.get("https://api.wegmans.io/products/categories?api-version=2018-10-18&subscription-key=#{@@key}") #{|response, request, result| response }
         result = JSON.parse(response.body)
@@ -28,7 +27,7 @@ class Item < ApplicationRecord
         end
         # if we want category class - .each  key=["name"]
     end
-
+    
     def self.sub_category
         Item.category.collect do |id| 
             response = HTTParty.get("https://api.wegmans.io/products/categories/#{id}?api-version=2018-10-18&subscription-key=#{@@key}") #{|response, request, result| response }
@@ -40,53 +39,61 @@ class Item < ApplicationRecord
     end
 
     def self.item_by_category
-        # key = ENV["WEGMAN_API_KEY"]
         Item.sub_category.collect do |sub_id|
             response = HTTParty.get("https://api.wegmans.io/products/categories/#{sub_id}?api-version=2018-10-18&subscription-key=#{@@key}")#{|response, request, result| response }
             result = JSON.parse(response.body)
-            # binding.pry
             return result["products"].collect do |product|
                 product["sku"]
             end
         end
     end
-
+    
     def self.info
-        # key = ENV["WEGMAN_API_KEY"]
         skus = Item.item_by_category.sample(40)
 
-        return skus.collect do |sku|
+        return skus.each do |sku|
             response = HTTParty.get("https://api.wegmans.io/products/#{sku}?api-version=2018-10-18&subscription-key=#{@@key}") #{|response, request, result| response }
             result = JSON.parse(response.body)
+            response_price = HTTParty.get("https://api.wegmans.io/products/#{sku}/prices?api-version=2018-10-18&subscription-key=#{@@key}")#{|response, request, result| response }
+            result_price = JSON.parse(response_price.body)
+            price = result_price["stores"][0]["price"]
             # binding.pry
-        end
-    end
+            
+            Item.find_or_create_by(
+                item_id: result["sku"], #sku number - product_url
+                name: result["name"], #product_url
+                sales_price: price, #price_url
+                description: result["descriptions"]["consumer"],
+                receipt_info: result["descriptions"]["receipt"],
+                inventory_quantity: rand(50..100),
+                image: result["tradeIdentifiers"][0]["images"][0]
+                # nutrition: result["nutrition"][0]["description"]
+            )
 
-    def self.create_item
-        items = Item.info
-        key = ENV["WEGMAN_API_KEY"]
-        # binding.pry
-        items.each do |item|
-            # response = HTTParty.get("https://api.wegmans.io/products/#{item["sku"]}/prices?api-version=2018-10-18&subscription-key=#{@@key}")#{|response, request, result| response }
-            response = RestClient.get("https://api.wegmans.io/products/756507/prices?api-version=2018-10-18", headers={'Subscription-Key': 'c455d00cb0f64e238a5282d75921f27e'}){|response, request, result| response }
             binding.pry
-            result = JSON.parse(response.body)
-            price = result["stores"][0]["price"]
-            Item.populate(item, price)
         end
+            
     end
 
-    def self.populate(response, price)
-        binding.pry
-        Item.find_or_create_by(
-            item_id: response["sku"], #sku number - product_url
-            name: respone["name"], #product_url
-            sales_price: price, #price_url
-            description: response["descriptions"]["consumer"],
-            receipt_info: respone["descriptions"]["receipt"],
-            inventory_quantity: rand(50..100),
-            image: response["tradeIdentifiers"][0]["images"][0],
-            nutrition: response["nutrition"][0]["description"]
-        )
-    end
+    # def self.create_item
+        # items = Item.item_by_category.sample(40)
+        # items.each do |item|
+            # response = RestClient.get("https://api.wegmans.io/products/756507/prices?api-version=2018-10-18", headers={'Subscription-Key': 'c455d00cb0f64e238a5282d75921f27e'}){|response, request, result| response }
+            # price = result["stores"][0]["price"]
+        # end
+    # end
+
+    # def self.populate(response, price)
+    #     binding.pry
+    #     Item.find_or_create_by(
+    #         item_id: response["sku"], #sku number - product_url
+    #         name: respone["name"], #product_url
+    #         sales_price: price, #price_url
+    #         description: response["descriptions"]["consumer"],
+    #         receipt_info: respone["descriptions"]["receipt"],
+    #         inventory_quantity: rand(50..100),
+    #         image: response["tradeIdentifiers"][0]["images"][0],
+    #         nutrition: response["nutrition"][0]["description"]
+    #     )
+    # end
 end
